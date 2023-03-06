c32e29ec53444e2b3b92b1f28bc7bc8315426f25
tranquilo-01 <krzys.dziechciarz@gmail.com>
2023-03-06 11:54:13 +0100

    initial commit

---
+++ lab01/lab/DynamicLibrary/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+    printMessage();
+    printf("podaj liczbe\n");
+    scanf("%d", &n);
+    printxtimes(n);
+    return 0;
+}   
---
+++ lab01/lab/StaticLibrary/stringLibrary.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void printMessage(){
+    printf("Hello world\n");
+}
+
+void printxtimes(int n){
+    for(int i = 0; i < n; i++){
+        printf("napis %d\n", i);
+    }
+}
