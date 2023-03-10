c6e25e61cc2f62e7de495e09219f81a64fb6541e
tranquilo-01 <krzys.dziechciarz@gmail.com>
2023-03-10 14:17:45 +0100

    lab02 init

---
+++ lab02/zad01/zad1lib.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void changeLetter(char oldacsii, char newascii, char* inpath, char* outpath){
+    
+}
+
+
+
+int main(int argc, char* argv[]){
+    if (argc != 5){
+        printf("Wrong number of arguments [%d], 4 args expected", argc-1);
+    }
+}
