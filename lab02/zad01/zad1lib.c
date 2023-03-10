adace1d6cb1376e6bda7a89cad9fdbde753e2170
tranquilo-01 <krzys.dziechciarz@gmail.com>
2023-03-10 14:28:57 +0100

    after lab

--- lab02/zad01/zad1lib.c
+++ lab02/zad01/zad1lib.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
 
 void changeLetter(char oldacsii, char newascii, char* inpath, char* outpath){
-    
+    FILE* infile = fopen(inpath, "r");
+    FILE* outfile = fopen(outpath, "w");
+
+    char buff;
+
+    while ((buff = fgetc(infile)) != EOF)
+        {
+            printf("%c", buff);
+        }
+
 }
 
 
 
 int main(int argc, char* argv[]){
-    if (argc != 5){
-        printf("Wrong number of arguments [%d], 4 args expected", argc-1);
-    }
+    // if (argc != 5){
+    //     printf("Wrong number of arguments [%d], 4 args expected", argc-1);
+    // }
+
+    changeLetter('a', 'b', argv[1], 'aa');
 }
