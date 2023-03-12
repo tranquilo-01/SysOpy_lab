6e5d8f8842d1ffd7913448bed99b4282d66c5c07
tranquilo-01 <krzys.dziechciarz@gmail.com>
2023-03-12 17:57:58 +0100

    reading, changing and writing works

--- lab02/zad01/zad1lib.c
+++ lab02/zad01/zad1lib.c
@@ -1,24 +1,47 @@
 #include <stdio.h>
 
-void changeLetter(char oldacsii, char newascii, char* inpath, char* outpath){
-    FILE* infile = fopen(inpath, "r");
-    FILE* outfile = fopen(outpath, "w");
+void changeLetter(char oldacsii, char newascii, char *inpath, char *outpath)
+{
+    // tworzenie wskaznikow i otwieranie plikow
+    FILE *inFile = fopen(inpath, "r");
+    FILE *outFile = fopen(outpath, "w");
+    char ch;
 
-    char buff;
+    // test czy sie otwarly
+    if (inFile == NULL || outFile == NULL)
+    {
+        printf("File is not available \n");
+        return;
+    }
 
-    while ((buff = fgetc(infile)) != EOF)
-        {
-            printf("%c", buff);
+    // czytanie po charze
+    while ((ch = fgetc(inFile)) != EOF)
+    {
+        // zamiana
+        if(ch == oldacsii){
+            ch = newascii;
         }
+        // zapis
+        fputc(ch, outFile);
+    }
 
+    // zamkniecie plikow
+    fclose(inFile);
+    fclose(outFile);
 }
 
+int main(int argc, char **argv[])
+{
+    if (argc != 5)
+    {
+        printf("Wrong number of arguments [%d], 4 args expected", argc - 1);
+        return -1;
+    }
 
+    char inLetter = argv[1][0];
+    char outLetter = argv[2][0];
+    char* inputName = argv[3];
+    char* outputName = argv[4];
 
-int main(int argc, char* argv[]){
-    // if (argc != 5){
-    //     printf("Wrong number of arguments [%d], 4 args expected", argc-1);
-    // }
-
-    changeLetter('a', 'b', argv[1], 'aa');
+    changeLetter(inLetter, outLetter, inputName, outputName);
 }
