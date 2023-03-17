e4629be4ddc892df4deb13fdd457c91119245f8b
tranquilo-01 <krzys.dziechciarz@gmail.com>
2023-03-17 22:11:32 +0100

    zad01 done

--- lab03/zad01/zad01_fork.c
+++ lab03/zad01/zad01_fork.c
@@ -25,17 +25,34 @@
 // }
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
-int main(int argc, char **argv){
-    if(argc != 2){
-        printf("Wrong number of arguments [%d], 1 argument (number of processes to create) required");
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        printf("Wrong number of arguments [%d], 1 argument (number of processes to create) required\n", argc - 1);
         return 1;
     }
 
-    for(int i  = 0; i < argv[1]; i++){
-        fork();
-        printf("Proces dziecka: Proces rodzica ma pid:%d\n",(int)getppid());
-        printf("Proces dziecka: Proces dziecka ma pid:%d\n",(int)getpid());
+    int n = atoi(argv[1]);
+
+    for (int i = 0; i < n; i++) {
+        pid_t pid = fork();
+        if (pid == -1) {
+            perror("Error: fork failed\n");
+            return 1;
+        } else if (pid == 0) {
+            printf("[%d]: Pid procesu rodzica: %d, pid procesu dziecka: %d\n", i, (int)getppid(), (int)getpid());
+            return 0;
+        }
     }
+
+    for (int i = 0; i < n; i++) {
+        wait(NULL);
+    }
+
+    printf("Pid procesu macierzystego: %d, argv[1]: %d\n", (int)getpid(), n);
+
     return 0;
 }
