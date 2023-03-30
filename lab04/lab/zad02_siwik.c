74419e53fad0695c9ab58c51d06bd596086eb9b7
tranquilo-01 <krzys.dziechciarz@gmail.com>
2023-03-30 20:21:10 +0200

    fixed zad02

--- lab04/lab/zad02_siwik.c
+++ lab04/lab/zad02_siwik.c
@@ -2,26 +2,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <sys/types.h>
+#include <unistd.h>
 
 void handler(int sig) {
-    printf("ja cie ale sygnal");
+    printf("ja cie ale sygnal\n");
 }
 
-int main(){
+int main() {
     int pid;
-    pid  = fork();
+    pid = fork();
 
-    if(pid){
+    if (pid) {
         sleep(1);
-        while(1){
+        while (1) {
             sleep(1);
             kill(pid, SIGUSR1);
         }
-    }else{
+    } else {
         signal(SIGUSR1, handler);
-        while(1){
+        while (1) {
             sleep(1);
         }
     }
