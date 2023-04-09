c29681db8d503102ac35a51e779082b55c6ffb6a
tranquilo-01 <krzys.dziechciarz@gmail.com>
2023-04-09 19:18:48 +0200

    zda01_fork done

--- lab04/zad01/zad01_fork.c
+++ lab04/zad01/zad01_fork.c
@@ -2,43 +2,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 // handler wypisujacy sygnal
 void handle_signal(int sig) {
-    printf("Received signal %d\n", sig);
+    printf("(%d): Received signal: %d\n", getpid(), sig);
 }
 
-void check_pending(char* name) {
+void check_pending() {
     sigset_t pending;
     sigpending(&pending);
     if (sigismember(&pending, SIGUSR1)) {
-        printf("Signal %s pending\n", name);
+        printf("(%d): signal is pending\n", getpid());
     } else {
-        printf("No signal %s pending\n", name);
+        printf("(%d): no signal is pending\n", getpid());
     }
 }
 
-void set_signal(char* name, char* action) {
+void set_and_raise_signal(char* action) {
     struct sigaction sa;
     sigset_t mask;
     sigemptyset(&mask);
     sigaddset(&mask, SIGUSR1);
+
     if (strcmp(action, "ignore") == 0) {
         sigaction(SIGUSR1, NULL, &sa);
         sa.sa_handler = SIG_IGN;
         sigaction(SIGUSR1, &sa, NULL);
-        printf("Ignoring signal %s\n", name);
+        printf("(%d): raise(SIGUSR1)\n", getpid());
+        raise(SIGUSR1);
     } else if (strcmp(action, "handler") == 0) {
         sigaction(SIGUSR1, NULL, &sa);
         sa.sa_handler = handle_signal;
         sigaction(SIGUSR1, &sa, NULL);
-        printf("Setting handler for signal %s\n", name);
+        printf("(%d): raise(SIGUSR1)\n", getpid());
+        raise(SIGUSR1);
     } else if (strcmp(action, "mask") == 0) {
         sigprocmask(SIG_BLOCK, &mask, NULL);
-        printf("Masking signal %s\n", name);
+        printf("(%d): raise(SIGUSR1)\n", getpid());
+        raise(SIGUSR1);
     } else if (strcmp(action, "pending") == 0) {
-        check_pending(name);
+        sigprocmask(SIG_BLOCK, &mask, NULL);
+        printf("(%d): raise(SIGUSR1)\n", getpid());
+        raise(SIGUSR1);
+        check_pending();
     } else {
         fprintf(stderr, "Unknown action %s\n", action);
     }
@@ -50,21 +58,27 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    // ustawienie sygnalu w procesie przodka
-    set_signal("parent", argv[1]);
+    int parent_process = 1;
 
-    // wyslanie sygnalu do samego siebie (przodek)
-    raise(SIGUSR1);
+    // ustawienie sygnalu w procesie przodka
+    set_and_raise_signal(argv[1]);
+    fflush(NULL);
 
     // stworzenie potomka
-    if (fork() == -1) {
+    parent_process = fork();
+    if (parent_process == -1) {
         perror("Error while forking");
     }
 
-    if (strcmp(argv[1], "pending") != 0) {
-        raise(SIGUSR1);
+    if (!parent_process) {
+        if (strcmp(argv[1], "pending") == 0) {
+            check_pending();
+        } else {
+            printf("(%d): raise(SIGUSR1)\n", getpid());
+            raise(SIGUSR1);
+        }
+    } else {
+        wait(NULL);
     }
-    sleep(1);
-
     return 0;
 }
