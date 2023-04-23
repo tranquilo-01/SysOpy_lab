6c53b8e1a4ce17e06c2f222339fb74db8dd1a3b5
tranquilo-01 <krzys.dziechciarz@gmail.com>
2023-04-23 21:33:59 +0200

    zad02 lab done

--- lab06/lab/zad02/zad02_reader.c
+++ lab06/lab/zad02/zad02_reader.c
@@ -6,5 +6,26 @@
 #include "common.h"
 
 int main(int argc, char** argv) {
-    return 0;
+    // id kolejki przekazywane jako parametr
+    int mymsgq = atoi(argv[1]);
+    printf("%d\n", mymsgq);
+
+    // stworenie bufora do odebrania wiadomosci
+    struct msgbuf buff1;
+
+    // odebranie obu wiadomocsi i ich wypisanie
+    for (int i = 0; i < 2; i++) {
+        if (msgrcv(mymsgq, &buff1, sizeof(buff1.mtext), 0, IPC_NOWAIT) == -1) {
+            perror("reader: msgrcv error");
+            exit(EXIT_FAILURE);
+        }
+
+        printf("%s\n", buff1.mtext);
+    }
+
+    // usuniecie kolejki
+    if (msgctl(mymsgq, IPC_RMID, NULL) == -1) {
+        perror("reader: msgctl error");
+        exit(EXIT_FAILURE);
+    }
 }
--- lab06/lab/zad02/zad02_writer.c
+++ lab06/lab/zad02/zad02_writer.c
@@ -6,6 +6,37 @@
 #include "common.h"
 
 int main() {
-    printf("%d\n", MSG_SIZE);
+    // tworzenie unikalnego klucza do identyfikacji kolejki
+    key_t key = ftok(".", 'A');
+
+    if (key == -1) {
+        perror("writer: ftok error");
+        exit(EXIT_FAILURE);
+    }
+
+    // utworzenie nowej kolejki komunikatow uzywajac wygenerowanego klucza
+    int mymsgq = msgget(key, IPC_CREAT | 0666);
+    printf("%d\n", mymsgq);
+
+    if (mymsgq == -1) {
+        perror("writer: msgget error");
+        exit(EXIT_FAILURE);
+    }
+
+    // utworzenie i wypelnienie buforow na wiadomosci do kolejki
+    struct msgbuf msg1 = {1, "Hello world"};
+    struct msgbuf msg2 = {2, "Forward"};
+
+    // wyslanie pierwszej wiadomosci
+    if (msgsnd(mymsgq, &msg1, sizeof(msg1.mtext), IPC_NOWAIT) == -1) {
+        perror("writer: msgsnd error");
+        exit(EXIT_FAILURE);
+    }
+
+    // wyslanie drugiej wiadomosci
+    if (msgsnd(mymsgq, &msg2, sizeof(msg2.mtext), IPC_NOWAIT) == -1) {
+        perror("writer: msgsnd error");
+        exit(EXIT_FAILURE);
+    }
     return 0;
 }
