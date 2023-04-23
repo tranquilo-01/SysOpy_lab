9b285fd7ed727766e9a19b43521ba1c6d8b6dc56
tranquilo-01 <krzys.dziechciarz@gmail.com>
2023-04-23 20:46:40 +0200

    zad01 fixed

--- lab06/lab/zad01.c
+++ lab06/lab/zad01.c
@@ -1,4 +1,4 @@
-// Napisz program w ktrym utworzysz kolejke komiunikatow
+// Napisz program w ktorym utworzysz kolejke komiunikatow
 // wstaiwsz tam dwa komunikaty, "witaj swiecie" oraz "cala naprzod"
 // nastepnie odbierz wszystkie komunikaty z tej kolejki
 // zapisujac je do innego bufora i wypisz je na ekran
@@ -11,56 +11,60 @@
 #include <sys/msg.h>
 
 /* message buffer for msgsnd and msgrcv calls */
-struct msgbuf
-{
+struct msgbuf {
     long mtype;     /* typ komunikatu   */
     char mtext[64]; /* tresc komunikatu */
 };
 
-int main()
-{
+int main() {
+    // tworzenie unikalnego klucza do identyfikacji kolejki
     key_t key = ftok(".", 'A');
 
-    if (key == -1)
-    {
+    if (key == -1) {
         perror("ftok error");
         exit(EXIT_FAILURE);
     }
 
+    // utworzenie nowej kolejki komunikatow uzywajac wygenerowanego klucza
     int mymsgq = msgget(key, IPC_CREAT | 0666);
 
-    if (mymsgq == -1)
-    {
+    if (mymsgq == -1) {
         perror("msgget error");
         exit(EXIT_FAILURE);
     }
 
-    struct msgbuf msgnoo = {1, "Hello world"};
-    struct msgbuf msgnot = {2, "Forward"};
+    // utworzenie i wypelnienie buforow na wiadomosci do kolejki
+    struct msgbuf msg1 = {1, "Hello world"};
+    struct msgbuf msg2 = {2, "Forward"};
 
-    if (msgsnd(mymsgq, &msgnoo, sizeof(msgnoo), IPC_NOWAIT) == -1)
-    {
+    // wyslanie pierwszej wiadomosci
+    if (msgsnd(mymsgq, &msg1, sizeof(msg1.mtext), IPC_NOWAIT) == -1) {
         perror("msgsnd error");
         exit(EXIT_FAILURE);
     }
 
-    if (msgsnd(mymsgq, &msgnoo, sizeof(msgnot), IPC_NOWAIT) == -1)
-    {
+    // wyslanie drugiej wiadomosci
+    if (msgsnd(mymsgq, &msg2, sizeof(msg2.mtext), IPC_NOWAIT) == -1) {
         perror("msgsnd error");
         exit(EXIT_FAILURE);
     }
 
+    // stworenie bufora do odebrania wiadomosci
     struct msgbuf buff1;
+    // printf("msgbuf: %ld, msg1: %ld, msg2, %ld\n", sizeof(buff1), sizeof(msg1), sizeof(msg2));
 
-    if (msgrcv(mymsgq, &buff1, sizeof(buff1), 0, IPC_NOWAIT) == -1)
-    {
-        perror("msgrcv error");
-        exit(EXIT_FAILURE);
+    // odebranie obu wiadomocsi i ich wypisanie
+    for (int i = 0; i < 2; i++) {
+        if (msgrcv(mymsgq, &buff1, sizeof(buff1.mtext), 0, IPC_NOWAIT) == -1) {
+            perror("msgrcv error");
+            exit(EXIT_FAILURE);
+        }
+
+        printf("%s\n", buff1.mtext);
     }
 
-    printf("%s\n", buff1.mtext);
-
-    if(msgctl(mymsgq, IPC_RMID, NULL) == -1){
+    // usuniecie kolejki
+    if (msgctl(mymsgq, IPC_RMID, NULL) == -1) {
         perror("msgctl error");
         exit(EXIT_FAILURE);
     }
