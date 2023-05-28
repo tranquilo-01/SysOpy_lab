2da7ef7f0df1bd3db53719ef37d356b2988b3477
tranquilo-01 <krzys.dziechciarz@gmail.com>
2023-05-28 18:15:31 +0200

    copied zad03 from lab materials

---
+++ lab10/lab/zad03_server.c
@@ -0,0 +1,56 @@
+// Domena internetowa, protokol polaczeniowy
+
+#include <arpa/inet.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <limits.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/un.h>
+#include <unistd.h>
+
+#define PORT 7777
+
+int main() {
+    int fd = -1;
+    if ((fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        printf("Error creating socket\n");
+    }
+
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(PORT);
+    addr.sin_addr.s_addr = inet_addr("0.0.0.0");
+    addr.sin_zero[0] = '\0';
+    if (bind(fd, (struct sockaddr*)&addr, sizeof(struct sockaddr)) ==
+        -1) {
+        printf("Error binding\n");
+    }
+
+    if (listen(fd, 3) == -1) {
+        printf("Error listening\n");
+    }
+
+    struct sockaddr_in cl_addr;
+    int cl_fd = -1;
+    socklen_t addr_size = sizeof(cl_addr);
+
+    if ((cl_fd = accept(fd, (struct sockaddr*)&cl_addr, &addr_size)) ==
+        -1) {
+        printf("Error accepting\n");
+    }
+
+    char buf[64];
+    if (read(cl_fd, buf, 64) == -1) {
+        printf("Error receiving message\n");
+    }
+    printf("Message from client: \"%s\"\n", buf);
+    shutdown(fd, SHUT_RDWR);
+    close(fd);
+
+    return 0;
+}
