7234a42cd13c33277a4c776cf874d83ed097d806
tranquilo-01 <krzys.dziechciarz@gmail.com>
2023-05-28 18:43:03 +0200

    kolos2przygotowania init

---
+++ kolos2przygotowania/kolos17_18/kol2/zad3/receiver.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define KEY "./queuekey"
+
+int main() {
+    sleep(1);
+    int val = 0;
+
+    /**********************************
+    Otworz kolejke systemu V "reprezentowana" przez KEY
+    **********************************/
+
+    while (1) {
+        /**********************************
+        Odczytaj zapisane w kolejce wartosci i przypisz je do zmiennej val
+        obowiazuja funkcje systemu V
+        ************************************/
+
+        printf("%d square is: %d\n", val, val * val);
+    }
+
+    /*******************************
+    posprzataj
+    ********************************/
+
+    return 0;
+}
---
+++ kolos2przygotowania/kolos17_18/kol2/zad3/sender.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+
+#define KEY "./queuekey"
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        printf("Not a suitable number of program parameters\n");
+        return (1);
+    }
+
+    /******************************************************
+    Utworz kolejke komunikatow systemu V "reprezentowana" przez KEY
+    Wyslij do niej wartosc przekazana jako parametr wywolania programu
+    Obowiazuja funkcje systemu V
+    ******************************************************/
+
+    return 0;
+}
